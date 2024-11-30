46fe56390abb1c19e8d804c8a2d606a0c7e05fc6
Erlend Storsve <erlendstorsve@gmail.com>
2024-11-30 17:49:56 +0100

    feat: follow python naming conventions

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 vec = pygame.math.Vector2
-running = True
 
 # images
 bg = pygame.image.load("images/background.jpg")
 
-gameStarted = False
-gameOver = False
+running = True
+game_started = False
+game_over = False
 difficulty = 1
 last_game_ended = 0
 
@@ -26,33 +26,36 @@ player = Player()
 pipes = pygame.sprite.Group()
 score_hitboxes = pygame.sprite.Group()
 
-def spawnPipe():
+
+def spawn_pipe():
     height = random.randint(300, 600)
-    bottomPipe = Pipe(height, bottom=True)
-    topPipe = Pipe(height - 250, bottom=False)
-    pipes.add(bottomPipe)
-    pipes.add(topPipe)
+    bottom_pipe = Pipe(height, bottom=True)
+    top_pipe = Pipe(height - 250, bottom=False)
+    pipes.add(bottom_pipe)
+    pipes.add(top_pipe)
 
     score_hitbox = ScoreHitbox()
     score_hitboxes.add(score_hitbox)
 
 
 # events
-spawnEvent = pygame.USEREVENT+1
-pygame.time.set_timer(spawnEvent, 1000 - ((difficulty - 1) * 100))
+spawn_event = pygame.USEREVENT + 1
+pygame.time.set_timer(spawn_event, 1000 - ((difficulty - 1) * 100))
 
-difficultyEvent = pygame.USEREVENT+2
-pygame.time.set_timer(difficultyEvent, 10000)
+difficulty_event = pygame.USEREVENT + 3
+pygame.time.set_timer(difficulty_event, 10000)
 
-def restart(): 
-    global player, gameStarted, gameOver, difficulty, clock, last_game_ended
 
-    gameStarted = False
-    gameOver = False
+def restart():
+    global player, game_started, game_over, difficulty, clock, last_game_ended
+
+    game_started = False
+    game_over = False
     difficulty = 1
     player = Player()
     pipes.empty()
-    # reset time
+    # TODO: reset time
+
 
 while running:
     # poll for events
@@ -60,11 +63,15 @@ while running:
     for event in events:
         if event.type == pygame.QUIT:
             running = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not gameStarted:
-            gameStarted = True
-        if event.type == spawnEvent and not gameOver and gameStarted:
-            spawnPipe()
-        if event.type == difficultyEvent and not gameOver and gameStarted:
+        if (
+            event.type == pygame.KEYDOWN
+            and event.key == pygame.K_SPACE
+            and not game_started
+        ):
+            game_started = True
+        if event.type == spawn_event and not game_over and game_started:
+            spawn_pipe()
+        if event.type == difficulty_event and not game_over and game_started:
             difficulty += 0.1
 
     # background image
@@ -80,10 +87,12 @@ while running:
         screen.blit(score_hitbox.surf, score_hitbox.rect)
 
     # game over state
-    if gameOver:
+    if game_over:
         font = pygame.font.Font(None, 50)
-        text = font.render("You suck! Press space to restart or e to exit", True, (0, 0, 0))
-        text_rect = text.get_rect(center=(WIDTH/2, HEIGHT/2))
+        text = font.render(
+            "You suck! Press space to restart or e to exit", True, (0, 0, 0)
+        )
+        text_rect = text.get_rect(center=(WIDTH / 2, HEIGHT / 2))
         screen.blit(text, text_rect)
 
         for event in events:
@@ -92,21 +101,19 @@ while running:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_e:
                 running = False
 
-    if gameStarted and not gameOver:
-        player.move(events, gameStarted)
+    if game_started and not game_over:
+        player.move(events, game_started)
         for pipe in pipes:
             pipe.move(difficulty)
 
     # game over state
     for pipe in pipes:
         if player.hitbox.colliderect(pipe.rect):
-            gameOver = True
+            game_over = True
     if player.pos.y >= 700 and player.vel.y >= 0:
-        gameOver = True
+        game_over = True
 
-    # flip() the display to put your work on screen
     pygame.display.flip()
-    # limit to 60 fps
     clock.tick(60)
 
 
--- sprites/pipe.py
+++ sprites/pipe.py
@@ -1,23 +1,24 @@
 import pygame
-from constants import WIDTH, HEIGHT
+from constants import WIDTH
 
 vec = pygame.math.Vector2
 
+
 class Pipe(pygame.sprite.Sprite):
     def __init__(self, height, bottom: bool):
-        super().__init__() 
+        super().__init__()
         self.surf = pygame.Surface((100, 1000))
         self.surf.fill((3, 163, 46))
         self.rect = self.surf.get_rect()
         self.bottom = bottom
-   
+
         self.pos = vec((WIDTH + 50, height))
         self.vel = vec(0, 0)
         if bottom:
             self.rect.midtop = self.pos
-        else: 
+        else:
             self.rect.midbottom = self.pos
-    
+
     def move(self, difficulty):
         self.pos.x -= 7 * difficulty
 
@@ -25,4 +26,4 @@ class Pipe(pygame.sprite.Sprite):
             self.rect.midtop = self.pos
         else:
             self.rect.midbottom = self.pos
-            
+
